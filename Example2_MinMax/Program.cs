c5c0346b8b862fe6ff6601edd52027aface4ab0b
PolinaLihane <polin@list.ru>
2022-08-06 16:58:45 +0300

    add Example2_MinMax

---
+++ Example2_MinMax/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+//Задача 2: Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+Clear();
+WriteLine("Введите 2 числа: ");
+int number1 = int.Parse(ReadLine());
+int number2 = int.Parse(ReadLine());
+
+if (number1>= number2) 
+{
+    WriteLine($"Наибольшее число: {number1} ; наименьшее число: {number2}");
+}  
+else   
+{
+    WriteLine($"Наибольшее число: {number2} ; наименьшее число: {number1}");
+}  
