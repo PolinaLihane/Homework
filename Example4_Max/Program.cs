3693fc2841b4c3d1c24e767c5494fb6182747970
PolinaLihane <polin@list.ru>
2022-08-06 17:11:51 +0300

    Решили 4 задачу с max

---
+++ Example4_Max/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+// Задача 4: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+Clear();
+WriteLine("Введите три числа: ");
+int number1 = int.Parse(ReadLine());
+int number2 = int.Parse(ReadLine());
+int number3 = int.Parse(ReadLine());
+int max = number1;
+
+if (number2>max) 
+{
+    max = number2;
+}
+if (number3>max)
+{
+    max = number3;
+}
+WriteLine($"Максимальное число: {max}");
