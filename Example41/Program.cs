bdcc0c23af15402a512839df09f5bba8614cc7a8
PolinaLihane <polin@list.ru>
2022-08-27 14:35:26 +0300

    это было сложно

---
+++ Example41/Program.cs
@@ -0,0 +1,20 @@
+﻿// 41
+//Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+using static System.Console;
+using System.Linq;
+Clear();
+WriteLine("Введите массив");
+int[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+PrintCount(array);
+
+void PrintCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    WriteLine(count);
+}
---
+++ Example43/Program.cs
@@ -0,0 +1,30 @@
+﻿// 43
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите b1");
+double b1 = int.Parse(ReadLine());
+WriteLine("Введите k1");
+double k1 = int.Parse(ReadLine());
+WriteLine("Введите b2");
+double b2 = int.Parse(ReadLine());
+WriteLine("Введите k2");
+double k2 = int.Parse(ReadLine());
+
+double x = Getx(b1, k1, b2, k2);
+double y = Gety(x, b2, k2);
+
+WriteLine($"Точка пересечения({x},{y})");
+
+double Getx(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+double Gety(double x, double b2, double k2)
+{
+    return k2 * x + b2;
+}
